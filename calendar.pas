675a41d008d3fedd65a53d69fc5bbf8d39082d3a
Djaoued2006 <149114067+Djaoued2006@users.noreply.github.com>
2023-10-28 23:35:00 +0100

    Add files via upload

---
+++ calendar.pas
@@ -0,0 +1,68 @@
+program makeCalendar;
+
+procedure calendar(nbrOfDays , startDay : integer);
+
+    var ptr , i : integer;
+
+    begin 
+
+        ptr := 0;
+        i := 1;
+
+        writeln('D  L  M  M  J  V  S');
+
+        while ptr < startDay do 
+
+            begin 
+                write('   ');
+                ptr := ptr + 1
+            end;
+
+        while True do 
+
+            begin 
+
+                if (i = nbrOfDays + 1) then break;
+
+                if (ptr = 7) then 
+
+                    begin   
+                        writeln();
+                        ptr := 0
+                    end; 
+
+                case i of 
+                    1..9 : write(i , '  ');
+                    else  write(i , ' ')
+                end;
+                
+                i := i + 1;
+                
+                ptr := ptr + 1;
+
+                   
+            end;
+        
+        writeln();
+        
+
+
+        
+    end;
+
+var startDay , nbrOfDays : integer;
+
+begin 
+
+    writeln('==========Calendar==========');
+    writeln();
+    write('Insert the start date (0-Sunday ... 6-Friday) : ');readln(startDay);
+    write('Insert the number of days in this month : ');readln(nbrOfDays);
+
+    writeln('Calendar');
+
+    writeln();
+
+    calendar(nbrOfDays , startDay);
+    
+end.
